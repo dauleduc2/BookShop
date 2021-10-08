7c0fb34b5c3877fd4257c4d33f9598715ca29a27
Loc <hoanglocst900@gmail.com>
2021-10-09 01:28:48 +0700

    UPDATE - add username field

--- BookShop/src/java/utils/Connector.java
+++ BookShop/src/java/utils/Connector.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package utils;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- *
- * @author Admin
- */
 public class Connector {
 
     public static Connection getConnection() {
