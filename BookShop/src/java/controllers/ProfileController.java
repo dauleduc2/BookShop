791d68ceba5f2f53de1ed3cd9e0b7c86bf2cb80c
Loc <hoanglocst900@gmail.com>
2021-10-18 03:09:44 +0700

    REMOVE - remove unuse import

--- BookShop/src/java/controllers/ProfileController.java
+++ BookShop/src/java/controllers/ProfileController.java
@@ -3,8 +3,6 @@ package controllers;
 import constant.Router;
 import daos.UserDAO;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
@@ -12,7 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import models.User;
 import utils.GetParam;
 import utils.Helper;
 
