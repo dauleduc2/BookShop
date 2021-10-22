302112f2a8a91031630b153779a20523d839d2a5
Loc <hoanglocst900@gmail.com>
2021-10-22 15:56:48 +0700

    ADD - add product detail controller

---
+++ BookShop/src/java/controllers/ProductDetailController.java
@@ -0,0 +1,58 @@
+package controllers;
+
+import constant.Router;
+import daos.ProductDAO;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import models.Product;
+import utils.Helper;
+
+@WebServlet(name = "ProductDetailController", urlPatterns = {"/product"})
+public class ProductDetailController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     */
+    protected boolean processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        response.setContentType("text/html;charset=UTF-8");
+        ProductDAO productDao = new ProductDAO();
+        Integer productId = Integer.parseInt(request.getParameter("productId"));
+
+        Product product = productDao.getProductById(productId);
+        if (product == null) {
+            Helper.setAttribute(request, 404, "Not found", "The requested URL was not found on this server");
+            return false;
+        }
+        request.setAttribute("product", product);
+        System.out.println("ABC");
+        return true;
+    }
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            if (!processRequest(request, response)) {
+                // forward on 404
+                request.getRequestDispatcher(Router.ERROR).forward(request, response);
+                return;
+            }
+            // forward on 200
+            request.getRequestDispatcher(Router.PRODUCT_DETAIL_PAGE).forward(request, response);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            // forward on 500
+            Helper.setAttribute(request, 500, "Something failed", "Please try again later");
+            request.getRequestDispatcher(Router.ERROR).forward(request, response);
+        }
+    }
+}
