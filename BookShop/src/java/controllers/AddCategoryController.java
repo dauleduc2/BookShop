86448e731c984a4c6c9c101cc0749926c73c0941
Bannanroc <60995744+Bannanroc@users.noreply.github.com>
2021-10-24 14:45:53 +0700

    UPDATE - check name existed

--- BookShop/src/java/controllers/AddCategoryController.java
+++ BookShop/src/java/controllers/AddCategoryController.java
@@ -36,16 +36,28 @@ public class AddCategoryController extends HttpServlet {
         boolean isTrue = true;
 
         String name = GetParam.getStringParam(request, "category", "Category's Name", 1, 50, null);
+
         if (name == null) {
             isTrue = false;
         }
 
+        if (categoryDao.getCategoryByName(name) != null) {
+            request.setAttribute("categoryError", "This Category's Name was existed.");
+            isTrue = false;
+        }
+
+        //check error occur
         if (!isTrue) {
             return false;
         }
 
+        //add new category
         Category category = new Category(null, name, null);
         categoryDao.addNewCategory(category);
+
+        //send success message
+        request.setAttribute("successMessage", "Change profile successful.");
+
         return true;
     }
 
@@ -82,7 +94,7 @@ public class AddCategoryController extends HttpServlet {
                 return;
             }
             // forward on 200
-            response.sendRedirect(Router.HOME_CONTROLLER);
+            this.doGet(request, response);
         } catch (Exception e) {
             // forward on 500
             System.out.println(e.getMessage());
