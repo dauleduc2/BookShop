227f6415f445c9ae0b1926e7970418f72ddbc510
Bannanroc <60995744+Bannanroc@users.noreply.github.com>
2021-10-25 20:58:00 +0700

    DELETE - delete not use class

--- BookShop/src/java/daos/CategoryDAO.java
+++ BookShop/src/java/daos/CategoryDAO.java
@@ -3,7 +3,6 @@ package daos;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import models.Category;
 import utils.Connector;
 
--- BookShop/src/java/daos/ProductDAO.java
+++ BookShop/src/java/daos/ProductDAO.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-import java.util.Date;
 import models.Product;
 import utils.Connector;
 
