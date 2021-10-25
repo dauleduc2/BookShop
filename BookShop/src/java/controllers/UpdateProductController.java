2bd1529ce6735cf2f28d072505cf2d976b9fe67a
Duc Dau <dauleduc2@gmail.com>
2021-10-25 21:33:50 +0700

    REFACTOR - fix some line code

--- BookShop/src/java/controllers/UpdateProductController.java
+++ BookShop/src/java/controllers/UpdateProductController.java
@@ -1,59 +1,13 @@
-<<<<<<< HEAD
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package controllers;
-
-import java.io.IOException;
-import java.io.PrintWriter;
-=======
 package controllers;
 
 import constant.Router;
 import daos.ProductDAO;
 import java.io.IOException;
->>>>>>> loc
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-<<<<<<< HEAD
-import constant.Router;
-
-/**
- *
- * @author Admin
- */
-@WebServlet(name = "UpdateProductController", urlPatterns = {"/" + Router.UPDATE_PRODUCT_CONTROLLER})
-public class UpdateProductController extends HttpServlet {
-
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
-    }
-
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
-        request.getRequestDispatcher(Router.UPDATE_PRODUCT_PAGE).forward(request, response);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
-    }
-
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
-=======
 import models.Product;
 import utils.GetParam;
 import utils.Helper;
@@ -152,5 +106,4 @@ public class UpdateProductController extends HttpServlet {
         }
     }
 
->>>>>>> loc
 }
