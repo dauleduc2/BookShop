fa59db30bcdd5bfd09223ba3a7df77ff4d743f09
Loc <hoanglocst900@gmail.com>
2021-11-02 22:35:41 +0700

    ADD - show product in category

--- BookShop/src/java/constant/Router.java
+++ BookShop/src/java/constant/Router.java
@@ -2,32 +2,33 @@ package constant;
 
 public class Router {
 
-	// jsp file mapping
-	public static final String LOGIN_PAGE = "/WEB-INF/view/loginPage.jsp";
-	public static final String ERROR = "/WEB-INF/view/commonView/errorPage.jsp";
-	public static final String HOME_PAGE = "/WEB-INF/view/home.jsp";
-	public static final String REGISTER_PAGE = "/WEB-INF/view/registerPage.jsp";
-	public static final String ME_PAGE = "WEB-INF/view/mePage.jsp";
-	public static final String CHANGE_PASSWORD_PAGE = "WEB-INF/view/changePassword.jsp";
-	public static final String ADD_PRODUCT_PAGE = "WEB-INF/view/addNewProductPage.jsp";
-	public static final String PRODUCT_DETAIL_PAGE = "WEB-INF/view/productDetailPage.jsp";
-	public static final String UPDATE_PRODUCT_PAGE = "WEB-INF/view/updateProductPage.jsp";
-	public static final String ADD_CATEGORY_PAGE = "WEB-INF/view/addCategoryPage.jsp";
-	public static final String CART_PAGE = "WEB-INF/view/cartPage.jsp";
-	public static final String SHOW_ORDERS_PAGE = "WEB-INF/view/showOrderPage.jsp";
-	// router mapping
-	public static final String LOGIN_CONTROLLER = "login";
-	public static final String REGISTER_CONTROLLER = "register";
-	public static final String CHANGE_PASSWORD_CONTROLLER = "password";
-	public static final String PROFILE_CONTROLLER = "me";
-	public static final String SIGNOUT_CONTROLLER = "signout";
-	public static final String HOME_CONTROLLER = "home";
-	public static final String ADD_PRODUCT_CONTROLLER = "addbook";
-	public static final String PRODUCT_DETAIL_CONTROLLER = "product";
-	public static final String UPDATE_PRODUCT_CONTROLLER = "updateproduct";
-	public static final String ADD_CATEGORY_CONTROLLER = "addcategory";
-	public static final String CART_CONTROLLER = "cart";
-	public static final String REMOVE_PRODUCT_CONTROLLER = "remove";
-	public static final String CHECKOUT_CONTROLLER = "checkout";
-	public static final String SHOW_ORDERS_CONTROLLER = "orders";
+    // jsp file mapping
+    public static final String LOGIN_PAGE = "/WEB-INF/view/loginPage.jsp";
+    public static final String ERROR = "/WEB-INF/view/commonView/errorPage.jsp";
+    public static final String HOME_PAGE = "/WEB-INF/view/home.jsp";
+    public static final String REGISTER_PAGE = "/WEB-INF/view/registerPage.jsp";
+    public static final String ME_PAGE = "WEB-INF/view/mePage.jsp";
+    public static final String CHANGE_PASSWORD_PAGE = "WEB-INF/view/changePassword.jsp";
+    public static final String ADD_PRODUCT_PAGE = "WEB-INF/view/addNewProductPage.jsp";
+    public static final String PRODUCT_DETAIL_PAGE = "WEB-INF/view/productDetailPage.jsp";
+    public static final String UPDATE_PRODUCT_PAGE = "WEB-INF/view/updateProductPage.jsp";
+    public static final String ADD_CATEGORY_PAGE = "WEB-INF/view/addCategoryPage.jsp";
+    public static final String CART_PAGE = "WEB-INF/view/cartPage.jsp";
+    public static final String SHOW_ORDERS_PAGE = "WEB-INF/view/showOrderPage.jsp";
+    // router mapping
+    public static final String LOGIN_CONTROLLER = "login";
+    public static final String REGISTER_CONTROLLER = "register";
+    public static final String CHANGE_PASSWORD_CONTROLLER = "password";
+    public static final String PROFILE_CONTROLLER = "me";
+    public static final String SIGNOUT_CONTROLLER = "signout";
+    public static final String HOME_CONTROLLER = "home";
+    public static final String ADD_PRODUCT_CONTROLLER = "addbook";
+    public static final String PRODUCT_DETAIL_CONTROLLER = "product";
+    public static final String UPDATE_PRODUCT_CONTROLLER = "updateproduct";
+    public static final String ADD_CATEGORY_CONTROLLER = "addcategory";
+    public static final String CART_CONTROLLER = "cart";
+    public static final String REMOVE_PRODUCT_CONTROLLER = "remove";
+    public static final String CHECKOUT_CONTROLLER = "checkout";
+    public static final String SHOW_ORDERS_CONTROLLER = "orders";
+    public static final String SHOW_PRODUCT_IN_CATEGORY_CONTROLLER = "categoryproducts";
 }
