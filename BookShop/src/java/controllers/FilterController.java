85cd4b0dc29e7e62c507c616b8df663388ec3787
Loc <hoanglocst900@gmail.com>
2021-11-19 18:30:05 +0700

    REFACTOR - filter, search, product in order

--- BookShop/src/java/controllers/FilterController.java
+++ BookShop/src/java/controllers/FilterController.java
@@ -19,87 +19,87 @@ import utils.Helper;
 @WebServlet(name = "FilterController", urlPatterns = {"/" + Router.FILTER_CONTROLLER})
 public class FilterController extends HttpServlet {
 
-	/**
-	 * Processes requests for both HTTP <code>POST</code> methods.
-	 */
-	protected void getHandler(HttpServletRequest request, HttpServletResponse response)
-		throws Exception {
-		response.setContentType("text/html;charset=UTF-8");
-		CategoryDAO categoryDao = new CategoryDAO();
-		ArrayList<Category> categories = categoryDao.getAllCategory();
-		request.setAttribute("categories", categories);
-	}
+    /**
+     * Processes requests for both HTTP <code>POST</code> methods.
+     */
+    protected void getHandler(HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        response.setContentType("text/html;charset=UTF-8");
+        CategoryDAO categoryDao = new CategoryDAO();
+        ArrayList<Category> categories = categoryDao.getAllCategory();
+        request.setAttribute("categories", categories);
+    }
 
-	/**
-	 * Handles the HTTP <code>GET</code> method.
-	 */
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-		throws ServletException, IOException {
-		try {
-			getHandler(request, response);
-		} catch (Exception e) {
-			// forward on 500
-			System.out.println(e);
-			Helper.setAttribute(request, StatusCode.INTERNAL_SERVER_ERROR.getValue(), "Something failed", "Please try again later");
-			request.getRequestDispatcher(Router.ERROR).forward(request, response);
-		}
-		request.getRequestDispatcher("WEB-INF/view/testFilter.jsp").forward(request, response);
-	}
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            getHandler(request, response);
+        } catch (Exception e) {
+            // forward on 500
+            System.out.println(e);
+            Helper.setAttribute(request, StatusCode.INTERNAL_SERVER_ERROR.getValue(), "Something failed", "Please try again later");
+            request.getRequestDispatcher(Router.ERROR).forward(request, response);
+        }
+        request.getRequestDispatcher(Router.SEARCH_BOOK_PAGE).forward(request, response);
+    }
 
-	/**
-	 * Processes requests for both HTTP <code>POST</code> methods.
-	 */
-	protected boolean postHandler(HttpServletRequest request, HttpServletResponse response)
-		throws Exception {
-		response.setContentType("text/html;charset=UTF-8");
-		ProductDAO productDao = new ProductDAO();
+    /**
+     * Processes requests for both HTTP <code>POST</code> methods.
+     */
+    protected boolean postHandler(HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        response.setContentType("text/html;charset=UTF-8");
+        ProductDAO productDao = new ProductDAO();
 
-		// get params
-		Integer categoryId = GetParam.getIntParams(request, "categoryId", "category", 0, Integer.MAX_VALUE, 1033);
-		Float minPrice = GetParam.getFloatParams(request, "minPrice", "min price", 0, Float.MAX_VALUE, 0.0f);
-		Float maxPrice = GetParam.getFloatParams(request, "maxPrice", "max price", 0, Float.MAX_VALUE, Float.MAX_VALUE);
+        // get params
+        Integer categoryId = GetParam.getIntParams(request, "categoryId", "category", 0, Integer.MAX_VALUE, 1033);
+        Float minPrice = GetParam.getFloatParams(request, "minPrice", "min price", 0, Float.MAX_VALUE, 0.0f);
+        Float maxPrice = GetParam.getFloatParams(request, "maxPrice", "max price", 0, Float.MAX_VALUE, Float.MAX_VALUE);
 
-		// check price
-		if (minPrice > maxPrice) {
-			request.setAttribute("priceError", "Min price cannot greater than max price");
-			return false;
-		}
+        // check price
+        if (minPrice > maxPrice) {
+            request.setAttribute("priceError", "Min price cannot greater than max price");
+            return false;
+        }
 
-		// get categories
-		CategoryDAO categoryDao = new CategoryDAO();
-		ArrayList<Category> categories = categoryDao.getAllCategory();
-		request.setAttribute("categories", categories);
+        // get categories
+        CategoryDAO categoryDao = new CategoryDAO();
+        ArrayList<Category> categories = categoryDao.getAllCategory();
+        request.setAttribute("categories", categories);
 
-		// get products
-		ArrayList<Product> products = productDao.getProducts(categoryId, minPrice, maxPrice);
+        // get products
+        ArrayList<Product> products = productDao.getProducts(categoryId, minPrice, maxPrice);
 
-		// send products
-		request.setAttribute("products", products);
-		return true;
-	}
+        // send products
+        request.setAttribute("products", products);
+        return true;
+    }
 
-	/**
-	 * Handles the HTTP <code>POST</code> method.
-	 */
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-		throws ServletException, IOException {
-		try {
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
 
-			if (!postHandler(request, response)) {
-				// forward on 400
-				request.getRequestDispatcher("WEB-INF/view/testFilter.jsp").forward(request, response);
-				return;
-			}
-			// forward on 200
-			request.getRequestDispatcher("WEB-INF/view/testFilter.jsp").forward(request, response);
-		} catch (Exception e) {
-			// forward on 500
-			System.out.println(e);
-			Helper.setAttribute(request, StatusCode.INTERNAL_SERVER_ERROR.getValue(), "Something failed", "Please try again later");
-			request.getRequestDispatcher(Router.ERROR).forward(request, response);
-		}
-	}
+            if (!postHandler(request, response)) {
+                // forward on 400
+                request.getRequestDispatcher(Router.SEARCH_BOOK_PAGE).forward(request, response);
+                return;
+            }
+            // forward on 200
+            request.getRequestDispatcher(Router.SEARCH_BOOK_PAGE).forward(request, response);
+        } catch (Exception e) {
+            // forward on 500
+            System.out.println(e);
+            Helper.setAttribute(request, StatusCode.INTERNAL_SERVER_ERROR.getValue(), "Something failed", "Please try again later");
+            request.getRequestDispatcher(Router.ERROR).forward(request, response);
+        }
+    }
 
 }
--- BookShop/src/java/controllers/SearchBookController.java
+++ BookShop/src/java/controllers/SearchBookController.java
@@ -1,69 +1,55 @@
 package controllers;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 import constant.Router;
+import daos.CategoryDAO;
 import daos.ProductDAO;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import models.Category;
 import models.Product;
 import models.StatusCode;
 import utils.GetParam;
 import utils.Helper;
 
-/**
- *
- * @author Admin
- */
 @WebServlet(name = "java", urlPatterns = {"/" + Router.SEARCH_BOOK_CONTROLLER})
 public class SearchBookController extends HttpServlet {
 
-	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-		throws ServletException, IOException {
-		request.setCharacterEncoding("UTF-8");
-		response.setContentType("text/html;charset=UTF-8");
-		//get name to search
-//        String name = GetParam.getStringParam(request, "name", "name", 0, 99, null);
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
+        ProductDAO productDAO = new ProductDAO();
 
-	}
+        //get name to search
+        String name = GetParam.getStringParam(request, "name", "name", 0, 99, null);
 
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-		throws ServletException, IOException {
-		request.getRequestDispatcher(Router.SEARCH_BOOK_PAGE).forward(request, response);
-	}
+        // get products by given name
+        ArrayList<Product> products = productDAO.searchProductByName(name);
+        request.setAttribute("products", products);
 
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-		throws ServletException, IOException {
-		try {
-			processRequest(request, response);
-			//get name to search
-			String name = GetParam.getStringParam(request, "name", "name", 0, 99, null);
-			ProductDAO productDAO = new ProductDAO();
-			ArrayList<Product> products = productDAO.searchProductByName(name);
-			request.setAttribute("products", products);
-			request.getRequestDispatcher("WEB-INF/view/testFilter.jsp").forward(request, response);
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-			// forward on 500
-			Helper.setAttribute(request, StatusCode.INTERNAL_SERVER_ERROR.getValue(), "Something failed", "Please try again later");
-			request.getRequestDispatcher(Router.ERROR).forward(request, response);
-		}
-	}
+        // get categories for filter
+        CategoryDAO categoryDao = new CategoryDAO();
+        ArrayList<Category> categories = categoryDao.getAllCategory();
+        request.setAttribute("categories", categories);
+    }
 
-	@Override
-	public String getServletInfo() {
-		return "Short description";
-	}
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+            request.getRequestDispatcher(Router.SEARCH_BOOK_PAGE).forward(request, response);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            // forward on 500
+            Helper.setAttribute(request, StatusCode.INTERNAL_SERVER_ERROR.getValue(), "Something failed", "Please try again later");
+            request.getRequestDispatcher(Router.ERROR).forward(request, response);
+        }
+    }
 
 }
