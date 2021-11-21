e380f81b4fd550bd025946c9622a59200dafa576
Loc <hoanglocst900@gmail.com>
2021-11-21 15:02:30 +0700

    FIX - status 1 2 3 to 0 and 4

--- BookShop/src/java/daos/OrderDAO.java
+++ BookShop/src/java/daos/OrderDAO.java
@@ -162,7 +162,7 @@ public class OrderDAO {
                     Product product = productDao.getProductById(orderItem.getProductId());
                     productDao.updateProductQuantity(product.getQuantity() - orderItem.getQuantity(), orderItem.getProductId());
                 }
-            } else if (order.getStatus() != 0 && nStatus == 4) {
+            } else if (order.getStatus() != 0 && (nStatus == 4 || nStatus == 0)) {
                 OrderItemDAO orderItemDao = new OrderItemDAO();
                 ProductDAO productDao = new ProductDAO();
                 ArrayList<OrderItem> orderItems = orderItemDao.getOrderItemByOrderId(order.getOrderId());
